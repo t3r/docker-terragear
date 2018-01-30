aa48e03dd0b42dfecec12aa36c1f42d15ca96fe8
Torsten Dreyer <torsten@t3r.de>
2018-01-30 16:56:58 +0100

    Make the decode script a bit more intelligent

--- home/decode.sh
+++ home/decode.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
-DECODEARGS="--max-segment 400 --spat 9 53 10 54 --threads 2"
+usage() { echo "Usage: $0 [-i<inputfile>] [-i -] [-b<basedir>] [-f] [-d]" 1>&2; exit 1; }
 
+INPUT=""
+FORCE=""
+BASE="$HOME"
+
+while getopts ":i:b:fd" o; do
+  case "${o}" in
+    i)
+      INPUT="${OPTARG}"
+      ;;
+    b)
+      BASE="${OPTARG}"
+      ;;
+    f)
+      FORCE=1
+      ;;
+    d)
+      DEBUG=1
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ "${INPUT}" != "-" -a ! -r "${INPUT}" ]; then
+  echo "Can't read input: ${INPUT}"
+  exit 1
+fi
+
+DECODEARGS="--max-segment 400 --spat 9 53 10 54 --threads 2"
 DATASOURCE="PG:dbname=${PGDATABASE} host=${PGHOST} user=${PGUSER} port=${PGPORT}"
 DECODE="ogr-decode ${DECODEARGS}"
 
-WORK="$HOME/tg/work"
+WORK="${BASE}/tg/work"
 
-cat "$(dirname $0)/decode.dat" | while read line; do
+cat "${INPUT}" | while read line; do
   if [[ "$line" == \#* ]]; then
     continue
   fi
@@ -18,12 +49,17 @@ cat "$(dirname $0)/decode.dat" | while read line; do
 
   mkdir -p "${WORKDIR}"
   # skip 
-  if [ -a "${WORKDIR}/${LAYERNAME}" -a "${WORKDIR}/${LAYERNAME}" -nt "$(dirname $0)/decode.dat" ]; then
-    echo "Skipping existing ${LAYERNAME}"
-    continue
+  if [ -a "${WORKDIR}/${LAYERNAME}" -a "${WORKDIR}/${LAYERNAME}" -nt "${INPUT}" ]; then
+    if [ -z "$FORCE" ]; then
+      echo "Skipping existing ${LAYERNAME}"
+      continue
+    fi
   fi
 
   echo "Processing ${LAYERNAME}"
+  if [ ! -z "$DEBUG" ]; then
+    echo ${DECODE} --area-type "${AREATYPE}" ${EXTRAS} "${WORKDIR}" "${DATASOURCE}" "${LAYERNAME}"
+  fi
   ${DECODE} --area-type "${AREATYPE}" ${EXTRAS} "${WORKDIR}" "${DATASOURCE}" "${LAYERNAME}" || exit $?
   date > "${WORKDIR}/${LAYERNAME}" 
 done
